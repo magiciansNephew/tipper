36fc4c8304574a20bfa796b9345f84d3d570d6a0
pramod <limbupramod1@gmail.com>
2021-06-27 10:51:48 +0545

    basic functionality

--- app/src/main/java/com/example/tipper/MainActivity.kt
+++ app/src/main/java/com/example/tipper/MainActivity.kt
@@ -3,16 +3,43 @@ package com.example.tipper
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tipper.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calcButton.setOnClickListener { displayTip(calculateTip()) }
     }
 
-    fun calculateTip() {
-        binding.plainTextInput.text
+    private fun calculateTip(): Double {
+        var totalPrice = binding.plainTextInput.text.toString().toDoubleOrNull()
+        var tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_fifteen_percent  -> 0.15
+            R.id.option_eighteen_percent -> 0.18
+            else   -> 0.20
+        }
+        var tipAmount: Double
+
+        tipAmount = if (totalPrice != null) {
+            totalPrice * tipPercentage
+        }else{
+            0.0
+        }
+
+        if(binding.roundUpSwitch.isChecked){
+            tipAmount = kotlin.math.ceil(tipAmount)
+        }
+
+        return tipAmount
+    }
+
+    private fun displayTip(tip: Double){
+        var formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipAmount.text = getString(R.string.tip_amount, formattedTip)
     }
 }
